86a8fa11bdc9abd663fe76ea07ce592a869edfdd
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-04-27 10:02:09 -0400

    Create c++-cheatsheep.cpp

---
+++ c++-cheatsheep.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int array[] = {3, 6, 9};
+
+int main() {
+  cout << "hello";
+   for (int i = 0; i < 3; i++) {
+    cout << array[i] << "\n";
+   }
+  return 0;
+}
+
+main();
