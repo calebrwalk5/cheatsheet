6802ba2f22e05f87946bbe57144c42670d00c78a
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-04-27 10:07:08 -0400

    Update c++-cheatsheep.cpp

--- c++-cheatsheep.cpp
+++ c++-cheatsheep.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 
 int array[] = {3, 6, 9};
+int variable = 5;
+char string = "variable string";
+const int CONSTANT = 69;
 
 int main() {
-  cout << "hello";
+  cout << "hello\n";
+  cout << variable << "\n";
+  cout << string << "\n";
+  cout << CONSTANT << "\n";
    for (int i = 0; i < 3; i++) {
     cout << array[i] << "\n";
    }
